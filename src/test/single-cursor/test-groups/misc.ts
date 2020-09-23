61b1242d51b21c0e9897ff505665fe385502329c
L.Y. Sim <onlylys@protonmail.com>
2020-09-23 20:33:13 +0800

    Create new 'Miscellaneous' test group... [cont.]
    The 'Escape Leaper Mode' test case is moved into the newly created test
    group.

---
+++ src/test/single-cursor/test-groups/misc.ts
@@ -0,0 +1,44 @@
+import { TestCase, TestGroup } from '../../typedefs';
+import { ALICE_TEXT_1, range } from '../../utilities';
+
+const TEST_CASES: TestCase[] = [
+
+    // Test the 'Escape Leaper Mode' command.
+    //
+    // Note that the command is directly called instead of being triggered by a keypress. 
+    {
+        name: 'Escape Leaper Mode',
+        prelude: {
+            description: 'Insert 10 pairs',
+            actions: [
+                // Insert pairs between some text to simulate a typical usage scenario.
+                { kind: 'typeText',      text:        ALICE_TEXT_1                          },
+                { kind: 'moveCursors',   direction:   'up'                                  },
+                { kind: 'moveCursors',   direction:   'left', repetitions: 10               },
+                { kind: 'insertPair',    repetitions: 10                                    },
+                { kind: 'assertPairs',   pairs:       [ { line: 5, sides: range(79, 90) } ] },
+                { kind: 'assertCursors', cursors:     [ [5, 89] ]                           },
+            ]
+        },
+        actions: [
+
+            // Jump out of one pair first, just to simulate a more 'random' scenario.
+            { kind: 'leap' },
+
+            // This should remove all pairs from being tracked.
+            { kind: 'escapeLeaperMode'                                     },
+            { kind: 'assertPairs',    pairs:   [ { line: -1, sides: [] } ] },
+            { kind: 'assertCursors',  cursors: [ [5, 90] ]                 }
+        ]
+    },
+    
+];
+
+/**
+ * This test group contains an assortment of test cases that are not big enough to require their own 
+ * module. 
+ */
+export const MISC_TEST_GROUP: TestGroup = {
+    name: 'Miscellaneous Tests (Single Cursor)',
+    testCases: TEST_CASES
+};
