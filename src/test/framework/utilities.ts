8b1c3a3bb01201e608f1244c5f077f35d972e85f
L.Y. Sim <onlylys@protonmail.com>
2020-10-14 18:59:32 +0800

    Add a new function that waits on a callback.

--- src/test/framework/utilities.ts
+++ src/test/framework/utilities.ts
@@ -47,6 +47,13 @@ export async function wait(n: number): Promise<void> {
 }
 
 /**
+ * Halt the control flow until the `callback` yields `true`.
+ */
+export async function waitUntil(callback: () => boolean): Promise<void> {
+    while (!callback()) {}
+}
+
+/**
  * Zip two iterables together.
  */
 export function* zip<T>(a: Iterable<T>, b: Iterable<T>): Generator<[T, T], undefined, undefined> {
