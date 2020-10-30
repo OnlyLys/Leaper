8d5b99cea0dda086c58bd99a26a099df92daea49
L.Y. Sim <onlylys@protonmail.com>
2020-10-31 03:36:48 +0800

    Move position of import statement for visual consistency.

--- src/test/single-cursor/single-cursor.test.ts
+++ src/test/single-cursor/single-cursor.test.ts
@@ -1,9 +1,9 @@
+import { TestCategory } from '../utilities/framework';
 import { SINGLE_CURSOR_INTEGRATION_TEST_GROUP } from './test-groups/integration';
 import { SINGLE_CURSOR_LEAP_COMMAND_TEST_GROUP } from './test-groups/leap';
 import { SINGLE_CURSOR_ESCAPE_LEAPER_MODE_COMMAND_TEST_GROUP } from './test-groups/escape-leaper-mode';
 import { SINGLE_CURSOR_PAIR_INVALIDATION_TEST_GROUP } from './test-groups/pair-invalidation';
 import { SINGLE_CURSOR_TRACKING_TEST_GROUP } from './test-groups/tracking';
-import { TestCategory } from '../utilities/framework';
 import { SINGLE_CURSOR_DECORATIONS_TEST_GROUP } from './test-groups/decorations';
 
 const TEST_CATEGORY = new TestCategory({
