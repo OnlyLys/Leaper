78b710c3a194cd992d51897e5403829f9e0eb7d5
L.Y. Sim <onlylys@protonmail.com>
2020-11-05 17:48:24 +0800

    Expand single cursor decorations tests... [cont.]
    Add a new test case to check that decorations are not messed up when
    focus switching between visible text editors.

--- src/test/single-cursor/test-groups/decorations.ts
+++ src/test/single-cursor/test-groups/decorations.ts
@@ -1,3 +1,4 @@
+import { ViewColumn } from 'vscode';
 import { CompactClusters } from '../../utilities/compact';
 import { Executor, TestCase, TestGroup } from '../../utilities/framework';
 
@@ -43,15 +44,42 @@ async function preludeSetup(executor: Executor): Promise<void> {
 /**
  * Type some pairs into the active text editor then assert that decorations were properly applied. 
  * 
- * If `expectDecorations` is `'all'`, then this function will check that decorations were applied to
- * all pairs being tracked for the cursor. Otherwise if `expectDecorations` is `'nearest'`, then
- * the check will be that decorations are only applied to the 'most nested' pair (i.e. the pair 
- * nearest to the cursor).
+ * # `expectDecorations` Parameter
  * 
- * Note that this task expects the active text editor to at least have an effective `leaper.detectedPairs` 
+ * If `expectDecorations` is `'all'`, then this function will check that decorations were applied to
+ * all pairs being tracked for the cursor. 
+ * 
+ * Otherwise if `expectDecorations` is `'nearest'`, then the check will be that decorations are only 
+ * applied to the 'most nested' pair (i.e. the pair nearest to the cursor).
+ * 
+ * # Note on `leaper.detectedPairs` Requirement
+ * 
+ * This task expects the active text editor to at least have an effective `leaper.detectedPairs` 
  * value of:
  *      
  *     [ "()", "[]", "{}", "``", "''", "\"\"" ]
+ * 
+ * # Document State Afterwards
+ * 
+ * By the end of this function, the active text document will have the following state:
+ * 
+ * ```
+ * async function helloWorld(): Promise<string> {
+ *     return new Promise((resolve) => {
+ *         setTimeout(() => resolve('Hello World'), 1000);
+ *     });
+ * };
+ * 
+ * async function main(): Promise<void> {
+ *     await (
+ *          async () => { 
+ *              console.log({ hey: [ '😎', await helloWorld() ] }); 
+ *          }                                               ^(cursor position)
+ *     )();
+ * }
+ * ```
+ * 
+ * with a pair cluster at `{ line: 9, sides: [23, 24, 31, 55, 56, 58, 60, 61] }`.
  */
 async function sharedTask(executor: Executor, expectDecorations: 'all' | 'nearest'): Promise<void> {
 
@@ -421,33 +449,35 @@ async function sharedTask(executor: Executor, expectDecorations: 'all' | 'neares
     executor.assertCursors({ expect: [ [9, 56] ] });
 };
 
-
 /**
- * Test whether pairs are correctly decorated when `leaper.decorateAll` is `true`.
+ * Open a text document which has `leaper.decorateAll` enabled and show it in `viewColumn`.
  * 
- * All pairs are expected to be decorated.
+ * The `leaper.detectedPairs` configuration of the text document is also set to:
+ * 
+ *     [ "()", "[]", "{}", "``", "''", "\"\"" ]
+ * 
+ * The opened text editor will take focus.
  */
-const DECORATE_ALL_PAIRS_TEST_CASE = new TestCase({
-    name: 'Decorate All Pairs',
-    prelude: async (executor) => {
+async function openDecorateAllEnabledTextEditor(
+    executor:   Executor, 
+    viewColumn: ViewColumn
+): Promise<void> {
 
-        // Instead of using the provided text editor (which will have an effective `leaper.decorateAll` 
-        // value of `false`), we open the text document in Workspace 2 which has that configuration 
-        // enabled instead.
-        await executor.openFile({ rel: './workspace-2/text.ts' });
+    // The text document in Workspace 2 of the test workspace has `leaper.decorateAll` enabled.
+    await executor.openFile({ 
+        rel:        './workspace-2/text.ts', 
+        showOptions: { viewColumn }
+    });
 
-        // The `sharedTask` function used later on in this test case expects `leaper.detectedPairs` 
-        // to at least be enabled for these pairs.
-        await executor.setConfiguration({
-            partialName:           'detectedPairs',
-            value:                 [ "()", "[]", "{}", "``", "''", "\"\"" ],
-            targetWorkspaceFolder: 'workspace-2',
-        });
+    // The `sharedTask` function expects these pairs to be detected.
+    await executor.setConfiguration({
+        partialName:           'detectedPairs',
+        value:                 [ "()", "[]", "{}", "``", "''", "\"\"" ],
+        targetWorkspaceFolder: 'workspace-2',
+    });
 
-        await preludeSetup(executor);
-    },
-    task: async (executor) => sharedTask(executor, 'all')
-});
+    await preludeSetup(executor);
+}
 
 /**
  * Test whether pairs are correctly decorated when `leaper.decorateAll` is `false`.
@@ -461,6 +491,94 @@ const DECORATE_ONLY_NEAREST_PAIR_TEST_CASE = new TestCase({
 });
 
 /**
+ * Test whether pairs are correctly decorated when `leaper.decorateAll` is `true`.
+ * 
+ * All pairs are expected to be decorated.
+ */
+const DECORATE_ALL_PAIRS_TEST_CASE = new TestCase({
+    name: 'Decorate All Pairs',
+    prelude: async (executor) => openDecorateAllEnabledTextEditor(executor, ViewColumn.Active),
+    task: async (executor) => sharedTask(executor, 'all')
+});
+
+/**
+ * Make sure that existing decorations are not messed up when focus is switched between visible text 
+ * editors.
+ */
+const NOT_MESSED_UP_BY_FOCUS_SWITCHING = new TestCase({
+    name: 'Not Messed Up by Focus Switching',
+    prelude: async (executor) => {
+
+        // Setup the provided text editor (which has `leaper.decorateAll` disabled).
+        await preludeSetup(executor);
+
+        // Use the `sharedTask` function to type some pairs into the provided text document.
+        await sharedTask(executor, 'nearest');
+
+        // Open another text editor (which has `leaper.decorateAll` enabled) in a separate view 
+        // column, then set it up the same way.
+        await openDecorateAllEnabledTextEditor(executor, ViewColumn.Two);
+        await preludeSetup(executor);
+
+        // Reuse the `sharedTask` function to type the same pairs into the opened text document.
+        await sharedTask(executor, 'all');
+
+        // By the end of the prelude, we will have two text documents opened side-by-side with the 
+        // same content within them. 
+        //
+        // However, they are both decorated differently, with the one in view column 1 having 
+        // `leaper.decorateAll` disabled while the one in view column 2 has `leaper.decorateAll`
+        // enabled.
+    },
+    task: async (executor) => {
+
+        // First check decorations are valid for both text editors.
+        executor.assertPairs({
+            expect:      [ { line: 9, sides: [23, 24, 31, 55, 56, 58, 60, 61] } ],
+            decorations: 'all'
+        });
+        executor.assertPairs({
+            expect:      [ { line: 9, sides: [23, 24, 31, 55, 56, 58, 60, 61] } ],
+            decorations: 'nearest',
+            viewColumn:  ViewColumn.One
+        });
+
+        // Switch focus to view column 1 then check again.
+        await executor.focusLeftEditorGroup();
+        executor.assertPairs({
+            expect:      [ { line: 9, sides: [23, 24, 31, 55, 56, 58, 60, 61] } ],
+            decorations: 'nearest'
+        });
+        executor.assertPairs({
+            expect:      [ { line: 9, sides: [23, 24, 31, 55, 56, 58, 60, 61] } ],
+            decorations: 'all',
+            viewColumn:  ViewColumn.Two
+        });
+
+        // Switch focus back to view column 2 then check again.
+        await executor.focusRightEditorGroup();
+        executor.assertPairs({
+            expect:      [ { line: 9, sides: [23, 24, 31, 55, 56, 58, 60, 61] } ],
+            decorations: 'all'
+        });
+        executor.assertPairs({
+            expect:      [ { line: 9, sides: [23, 24, 31, 55, 56, 58, 60, 61] } ],
+            decorations: 'nearest',
+            viewColumn:  ViewColumn.One
+        });
+
+        // Close the text editor in view column 2 (which will yield focus to the text editor in 
+        // view column 1) then check again.
+        await executor.closeActiveEditor();
+        executor.assertPairs({
+            expect:      [ { line: 9, sides: [23, 24, 31, 55, 56, 58, 60, 61] } ],
+            decorations: 'nearest'
+        });
+    }
+
+});
+
+/**
  * The following test group tests whether decorations are properly applied for pairs in single 
  * cursor situation.
  */
@@ -468,7 +586,8 @@ export const SINGLE_CURSOR_DECORATIONS_TEST_GROUP = new TestGroup({
     name: 'Decorations',
     testCases: [
         DECORATE_ALL_PAIRS_TEST_CASE,
-        DECORATE_ONLY_NEAREST_PAIR_TEST_CASE
+        DECORATE_ONLY_NEAREST_PAIR_TEST_CASE,
+        NOT_MESSED_UP_BY_FOCUS_SWITCHING
     ]
 });
 
