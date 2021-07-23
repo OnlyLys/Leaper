dd78e4c3f3defd697d98d007acb3fbbbc42073dc
L.Y. Sim <onlylys@protonmail.com>
2021-07-24 00:27:00 +0800

    Rename test category.

--- src/tests/multi-cursor-behavior/index.test.ts
+++ src/tests/multi-cursor-behavior/index.test.ts
@@ -4,11 +4,11 @@ import { MULTI_CURSORS_INTEGRATION_TEST_GROUP } from './integration';
 /**
  * A collection of test groups that test how the engine behaves when there are multiple cursors.
  */
-const MULTI_CURSOR_BEHAVIOR_TESTS = new TestCategory(
-    'Multi Cursor Behavior Tests',
+const MULTI_CURSORS_BEHAVIOR_TESTS = new TestCategory(
+    'Multi Cursors Behavior Tests',
     [
-        MULTI_CURSORS_INTEGRATION_TEST_GROUP
+        MULTI_CURSORS_INTEGRATION_TEST_GROUP,
     ]
 );
 
-MULTI_CURSOR_BEHAVIOR_TESTS.run();
+MULTI_CURSORS_BEHAVIOR_TESTS.run();
